681a56dfa081f96d558b8e48045f3fbba04be249
kuakobayashiriku0609 <85724662+kuakobayashiriku0609@users.noreply.github.com>
2021-10-15 14:40:43 +0900

    Update script01.js

--- sample/js/script01.js
+++ sample/js/script01.js
@@ -1,6 +1,14 @@
 $(function(){
 
+  let text1="おはようございます！"
+  let text2="おげんきですか？"
+  let num1=20;
+  let num=32;
+
+ function sayHello(){
+        $("#content").text("こんにちは!")
+ }
 
 
 
-})
+    })
